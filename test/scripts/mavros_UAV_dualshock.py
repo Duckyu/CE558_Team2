bd7261def71613ad16408bb6c7f7d22d6605f9ac
Duckyu Choi <duckyu@kaist.ac.kr>
2020-10-10 20:11:56 +0900

    test code debug

--- test/scripts/mavros_UAV_dualshock.py
+++ test/scripts/mavros_UAV_dualshock.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 import rospy
-from mavros_msgs.srv import CommandBool
+from mavros_msgs.srv import CommandBool, SetMode
 from scipy.spatial.transform import Rotation as R
 from std_msgs.msg import String
-from mavros_msgs.msg import PositionTarget, State
+from mavros_msgs.msg import PositionTarget, State, ExtendedState
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import PoseStamped
 
@@ -20,8 +20,11 @@ height_snap_resolution = 0.25 # in meter
 takeoff_height = 1.5
 # mode = 'null'
 setpoint_msg = PositionTarget()
+hover_flag = False
+hover = PositionTarget()
 pose = PoseStamped()
 state = State()
+ext_state = ExtendedState()
 
 # mavros_msgs/CommandBool.srv
 # bool value
@@ -31,6 +34,7 @@ state = State()
 def sub_setup():
     joy_sub = rospy.Subscriber("/joy", Joy, joy_callback)
     state_sub = rospy.Subscriber("/mavros/state", State, state_callback)
+    ext_state_sub = rospy.Subscriber("/mavros/extended_state", ExtendedState, ext_state_callback)
     local_position_sub = rospy.Subscriber("/mavros/local_position/pose", PoseStamped, local_position_callback)
 
 def move(control_msg):
@@ -39,71 +43,86 @@ def move(control_msg):
 
 def takeoff():
     global pose
-    takeoff_point = PositionTarget()
-    takeoff_point.type_mask = position_mask
-    takeoff_point.position = pose.pose.position
-    takeoff_point.position.z = takeoff_point.position.z + takeoff_height
-    move(takeoff_point)
-    arming_srv = rospy.ServiceProxy('/mavros/cmd/arming', mavros_msgs.srv.CommandBool)
+    # setpoint_msg = PositionTarget()
+    setpoint_msg.type_mask = position_mask
+    setpoint_msg.position = pose.pose.position
+    setpoint_msg.position.z = setpoint_msg.position.z + takeoff_height
+    move(setpoint_msg)
+    arming_srv = rospy.ServiceProxy('/mavros/cmd/arming', CommandBool)#mavros_msgs.srv.
     arm_result = arming_srv(True)
+    mode_srv = rospy.ServiceProxy('/mavros/set_mode', SetMode)#mavros_msgs.srv.
+    mode_result = mode_srv(0,"OFFBOARD")
 
-def joy_callback(self, joy_data):
+def joy_callback(joy_data): # self, 
     global setpoint_msg
     global pose
+    global hover_flag
     #joy_data.axes[]
     #joy_data.buttons[]
     if joy_data.buttons[10] == 1:
-        if pose.pose.position.z > 0.05:
-            rospy.loginfo("already flying")
-        else :
+        if ext_state.landed_state == ExtendedState().LANDED_STATE_ON_GROUND:
+            rospy.loginfo("attempting takeoff")
             takeoff()
+        else:
+            rospy.loginfo("already flying : %lf", pose.pose.position.z)
     elif joy_data.axes[6] != 0 or joy_data.axes[7] != 0:
         setpoint_msg.type_mask = position_mask
+        rospy.loginfo("type_mask : %d", setpoint_msg.type_mask)
         if joy_data.axes[6] != 0:
-            current_quat = [pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w]
-            current_angle = R.from_quat(current_quat)
+            current_quat = [pose.pose.orientation.x, pose.pose.orientation.y, pose.pose.orientation.z, pose.pose.orientation.w]
+            current_rmat = R.from_quat(current_quat)
             current_angle = current_rmat.as_euler('zyx', degrees=True)
             yaw_target = current_angle[0]+(joy_data.axes[6] == 1)*angle_snap_resolution-current_angle[0]%angle_snap_resolution
             setpoint_msg.yaw = yaw_target
             setpoint_msg.position = pose.pose.position
         elif joy_data.axes[7] != 0:
-            current_quat = [pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w]
-            current_angle = R.from_quat(current_quat)
+            current_quat = [pose.pose.orientation.x, pose.pose.orientation.y, pose.pose.orientation.z, pose.pose.orientation.w]
+            current_rmat = R.from_quat(current_quat)
             current_angle = current_rmat.as_euler('zyx', degrees=True)
             setpoint_msg.yaw = current_angle[0]
             setpoint_msg.position = pose.pose.position
             setpoint_msg.position.z =  pose.pose.position.z + height_snap_resolution * joy_data.axes[7]
     elif joy_data.axes[0] != 0 or joy_data.axes[1] != 0 or joy_data.axes[3] != 0 or joy_data.axes[4] != 0:
         setpoint_msg.type_mask = velocity_mask
+        rospy.loginfo("type_mask : %d", setpoint_msg.type_mask)
         setpoint_msg.velocity.x = max_velocity * joy_data.axes[4]
         setpoint_msg.velocity.y = - max_velocity * joy_data.axes[3]
         setpoint_msg.velocity.z = max_velocity * joy_data.axes[1]
         setpoint_msg.yaw_rate = - max_velocity * joy_data.axes[0]
     else:
-        setpoint_msg.type_mask = velocity_mask
-        setpoint_msg.velocity.x = 0
-        setpoint_msg.velocity.y = 0
-        setpoint_msg.velocity.z = 0
-        setpoint_msg.yaw_rate = 0
+        hover_flag = True
 
-def state_callback(self, state_data):
+        
+def state_callback(data):# self, 
     global state
-    state = state_data
+    state = data
 
-def local_position_callback(self, local_position_data):
+def ext_state_callback(data):# self, 
+    global ext_state
+    ext_state = data
+
+def local_position_callback(data): # self, 
     global pose
-    pose = local_position_data
+    global hover_flag
+    pose = data
+    if hover_flag == True:
+        hover_flag = False
+        hover.type_mask = position_mask
+        hover.position = pose.pose.position
+    # rospy.loginfo("poss x,y,z: %lf, %lf, %lf", pose.pose.position.x, pose.pose.position.y, pose.pose.position.z)
 
 if __name__ == '__main__':
     # global pose
     # global setpoint_msg
     rospy.init_node('dualshock_UAV', anonymous=True)
-    rate = rospy.Rate(10)
+    setpoint_msg.coordinate_frame = 8;
     sub_setup()
 
-    try:
-        move(setpoint_msg)
-        rospy.spin()
-    except rospy.ROSInterruptException:
-        rospy.loginfo('Node terminated by pressing Ctrl+C!')
-        sys.exit(0)
+    while not rospy.is_shutdown():
+        try:
+            # rospy.loginfo("try")
+            move(setpoint_msg)
+            rospy.sleep(0.1)
+        except rospy.ROSInterruptException:
+            rospy.loginfo('Node terminated by pressing Ctrl+C!')
+            sys.exit(0)
